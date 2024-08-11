6b0d5d4c33564e6355745027654f1b260631b602
FusionWithArt-Identity <mrigadas30@gmail.com>
2024-08-12 01:44:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
 	if (password === correctPassword) {
 		document.getElementById('heading').innerHTML = 'Hello Mihir';
 		document.getElementById('heading').classList.add('loggedIn');
-		document.getElementById('Mihir').src = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi7anHeHwC4Ns77JkbsmjTTJhEdeHYjjQBxyROHpTaDwpEhcNAmUOwpfXhxHsBeLLVeFPpc4mKIRvxkyVXaV5119e5f7fCy3mLETBcWixEwY-MrCu8fIfm8AMGpdgr0AHopIp740oXS0_Q7Jg_GEWBdErCXAD2OnuSZwLzOszYpYEgPotAOQn92ZiQmQMEE/s1600/The%20Earth.png';
+		document.getElementById('Mihir').src = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiB7ktKS8T_T_evSSb27yW7mJ9ntd1rHlKW2bSCF23tTd47e7R_8phLRiEwo216Hqhw3gLkm7w9opaxKgA9tyR1fgTNSDWO-bQuMjC9rb-0Dsk-gVVzRkn3yk8RWM19EEfa7dWgfA0qAGj818aiiNtmNP03ZzsXOPhiIIywL2AC0mfPce-3DVLBj6YjMJ6s/s1600/paris-games-conclude-6753651837110568-la202124.gif';
 		document.getElementById('login-form').style.display = 'none';
 		document.getElementById('protected-content').style.display = 'block';
 		document.getElementById('subheading').style.display = 'block';
